fd27c453e93dab08cc259f21f9799792c88ae6aa
Moloko <moloko@folopay.ru>
2026-01-02 23:19:23 +0500

    заполнение hotfixes.item_sparse по классике 7.3.5
    также добавлены локализации НПС

---
+++ sql/updates/world/world_2026_01_02_007.sql
@@ -0,0 +1,6 @@
+UPDATE `creature_text` SET `Type` = 12 WHERE `CreatureID` = 23141 AND `GroupID` = 0 AND `ID` = 0;
+
+UPDATE `creature_text` SET `BroadcastTextID` = 49897 WHERE `CreatureID` = 49869 AND `GroupID` = 0 AND `ID` = 0;
+
+UPDATE `creature_template` SET `speed_walk` = 0.00001 WHERE `entry` = 522111;
+
